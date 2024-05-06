d87806eb874667c071b8ba35a964d46fac2494ce
Furkan Baytekin <furkanbaytekin@gmail.com>
2024-05-06 09:14:39 +0300

    reafactor: splitted cli class

--- src/api/cli.ts
+++ src/api/cli.ts
@@ -1,9 +1,14 @@
 import { shell } from "@tauri-apps/api"
 
-export class CLI {
+type Output = {
+  success: boolean
+  output: string
+}
+
+class CLI {
   private static executable = "./passenger-cli"
 
-  private static execute = async (command: string, args: string[]): Promise<shell.ChildProcess> =>
+  public static execute = async (command: string, args: string[]): Promise<shell.ChildProcess> =>
     await new shell.Command(
       "sh", ["-c",
       `${CLI.executable
@@ -12,38 +17,40 @@ export class CLI {
       }`]
     ).execute()
 
-  private static readOutput = (process: shell.ChildProcess): string =>
-    process.code === 0
-      ? process.stdout
-      : process.stderr
+  public static readOutput = (process: shell.ChildProcess): Output => ({
+    success: process.code === 0,
+    output: process.stdout
+  })
+}
 
-  public static login = async (passphrase: string): Promise<string> =>
+export default class Command {
+  public static login = async (passphrase: string): Promise<Output> =>
     CLI.readOutput(await CLI.execute("login", [passphrase]))
 
-  public static register = async (passphrase: string): Promise<string> =>
+  public static register = async (passphrase: string): Promise<Output> =>
     CLI.readOutput(await CLI.execute("register", [passphrase]))
 
-  public static reset = async (oldPassphrase: string, newPassphrase: string): Promise<string> =>
+  public static reset = async (oldPassphrase: string, newPassphrase: string): Promise<Output> =>
     CLI.readOutput(await CLI.execute("reset", [oldPassphrase, newPassphrase]))
 
-  public static fetchAll = async (jwt: string): Promise<string> =>
+  public static fetchAll = async (jwt: string): Promise<Output> =>
     CLI.readOutput(await CLI.execute("fetchAll", [jwt]))
 
-  public static query = async (jwt: string, keyword: string): Promise<string> =>
+  public static query = async (jwt: string, keyword: string): Promise<Output> =>
     CLI.readOutput(await CLI.execute("query", [jwt, keyword]))
 
-  public static fetch = async (jwt: string, uuid: string): Promise<string> =>
+  public static fetch = async (jwt: string, uuid: string): Promise<Output> =>
     CLI.readOutput(await CLI.execute("fetch", [jwt, uuid]))
 
-  public static create = async (jwt: string, data: string): Promise<string> =>
+  public static create = async (jwt: string, data: string): Promise<Output> =>
     CLI.readOutput(await CLI.execute("create", [jwt, data]))
 
-  public static update = async (jwt: string, uuid: string, json: string): Promise<string> =>
+  public static update = async (jwt: string, uuid: string, json: string): Promise<Output> =>
     CLI.readOutput(await CLI.execute("update", [jwt, uuid, json]))
 
-  public static delete = async (jwt: string, uuid: string): Promise<string> =>
+  public static delete = async (jwt: string, uuid: string): Promise<Output> =>
     CLI.readOutput(await CLI.execute("delete", [jwt, uuid]))
 
-  public static version = async (): Promise<string> =>
+  public static version = async (): Promise<Output> =>
     CLI.readOutput(await CLI.execute("version", []))
 }
