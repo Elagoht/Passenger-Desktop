c735962d79f0780d4734cc86d10e6f7b5311c570
Furkan Baytekin <furkanbaytekin@gmail.com>
2024-05-14 18:31:52 +0300

    refactor: optimize AnimatedRoutes component

--- src/lib/router/AnimatedRoutes.tsx
+++ src/lib/router/AnimatedRoutes.tsx
@@ -1,10 +1,7 @@
 import React from "react"
 import { Outlet, useLocation } from "react-router-dom"
 
-const AnimatedRoutes: React.FC = () => {
-  const location = useLocation()
-
-  return <Outlet key={location.pathname} />
-}
+const AnimatedRoutes: React.FC = () =>
+  <Outlet key={useLocation().pathname} />
 
 export default AnimatedRoutes
