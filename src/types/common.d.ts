79851d73ed6114773433574fc6bb0839326e5953
Furkan Baytekin <furkanbaytekin@gmail.com>
2024-05-16 18:26:21 +0300

    refactor: Update ListablePassphrase type to include additional properties

--- src/types/common.d.ts
+++ src/types/common.d.ts
@@ -1,17 +1,3 @@
-export type Passphrase = {
-  id: string
-  platform: string
-  url: string
-  username: string | null
-  email: string | null
-  passphrase: string
-  notes?: string
-  createdAt: string
-  updatedAt: string
-  lastAccessedAt: string
-  totalAccesses: number
-}
-
 export type ListablePassphrase = {
   id: string
   platform: string
@@ -21,4 +7,10 @@ export type ListablePassphrase = {
   createdAt: string
   updatedAt: string
   lastAccessedAt: string
+}
+
+export type Passphrase = ListablePassphrase & {
+  passphrase: string
+  notes?: string
+  totalAccesses: number
 }
