61678eac49e79baab5227a43439b964277be2f51
Furkan Baytekin <furkanbaytekin@gmail.com>
2024-05-25 01:38:35 +0300

    style removed empty line

--- src/components/layout/NavBar/index.tsx
+++ src/components/layout/NavBar/index.tsx
@@ -7,7 +7,6 @@ const NavBar: FC = () => {
   const { pathname } = useLocation()
 
   return <nav className="grid max-md:grid-cols-5 md:flex md:flex-col bg-tuatara-50 dark:bg-tuatara-900 max-md:w-full md:h-screen p-1 md:p-2 md:min-w-56 shadow-modal shadow-tuatara-200 dark:shadow-tuatara-950 z-10">
-
     <header className="flex items-center justify-center gap-2 p-2 mb-2 max-md:hidden">
       <img
         src="/icon.png"
