a557a957a0a149b4dcee42766714a870d7e175e2
Furkan Baytekin <furkanbaytekin@gmail.com>
2024-06-02 23:01:50 +0300

    fix: distribution graph values

--- src/components/(dashboard)/StrengthDistributionChart.tsx
+++ src/components/(dashboard)/StrengthDistributionChart.tsx
@@ -12,10 +12,10 @@ const StrengthDistributionChart: FC<IStrengthDistributionChartProps> = ({ streng
   // Calculate quantities of each strength of passphrases from 0 to 10
   const data = Array.from({ length: 11 }, (_, index) => ({
     label: index.toString(),
-    value: Object.keys(strengths).filter(key => strengths[key] === index).length
+    value: Object.keys(strengths).filter(key => strengths[key] === index - 2).length
   }))
 
-  const colors = Array.from({ length: 11 }, (_, index) => Strength.color(index))
+  const colors = Array.from({ length: 11 }, (_, index) => Strength.color(index - 2))
 
   return <article className="rounded-xl p-4 lg:col-span-2 lg:row-span-2 shadow shadow-tuatara-300 dark:shadow-tuatara-950 bg-tuatara-50 dark:bg-tuatara-900">
     <h2 className="text-lg font-semibold text-creamcan-500 mb-3">
