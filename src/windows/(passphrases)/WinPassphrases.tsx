8df47b86c97cfb0598389454963a46115c7814e2
Furkan Baytekin <furkanbaytekin@gmail.com>
2024-06-07 00:57:51 +0300

    chore: re-added white space

--- src/windows/(passphrases)/WinPassphrases.tsx
+++ src/windows/(passphrases)/WinPassphrases.tsx
@@ -3,17 +3,19 @@ import Window from "../../components/layout/Window"
 import PassphraseList from "../../components/(passphrases)/PassphraseList"
 
 const WinPassphrases: FC = () => {
-  return <Window className="flex flex-col gap-6 p-4">
-    <img
-      src="/safe.webp"
-      alt="Passphrases"
-      width={256}
-      height={256}
-      draggable="false"
-      className="mt-6 mx-auto max-h-[33%] aspect-square max-w-full object-contain"
-    />
+  return <Window>
+    <div className="flex flex-col gap-6">
+      <img
+        src="/safe.webp"
+        alt="Passphrases"
+        width={256}
+        height={256}
+        draggable="false"
+        className="mt-6 mx-auto max-h-[33%] aspect-square max-w-full object-contain"
+      />
 
-    <PassphraseList />
+      <PassphraseList />
+    </div>
   </Window>
 }
 
