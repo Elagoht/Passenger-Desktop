b4251dd85bb14f7c052bdd3d117c29e5d24b3c84
Furkan Baytekin <furkanbaytekin@gmail.com>
2024-06-07 01:09:24 +0300

    feat: go back button

--- src/windows/(settings)/constant-pairs/[key]/WinConstantPairForm.tsx
+++ src/windows/(settings)/constant-pairs/[key]/WinConstantPairForm.tsx
@@ -1,10 +1,25 @@
 import { FC } from "react"
 import ConstantPairForm from "../../../../components/(settings)/ConsantPairs/ConstantPairForm"
 import Window from "../../../../components/layout/Window"
+import { Link } from "react-router-dom"
+import { IconArrowLeft } from "@tabler/icons-react"
 
 const WinConstantPairForm: FC = () => {
   return <Window compact>
-    <h1>Constant Pair Form</h1>
+    <div className="flex gap-2 items-center mb-4">
+      <Link
+        draggable="false"
+        to="/settings/constant-pairs"
+        className="hover:bg-tuatara-200 dark:hover:bg-tuatara-800 p-2 hover:rounded-3xl transition-all duration-300"
+      >
+        <IconArrowLeft size={32} />
+      </Link>
+
+      <h1 className="text-2xl font-medium text-tuatara-900 dark:text-tuatara-50">
+        Constant Pair Details
+      </h1>
+    </div>
+
 
     <ConstantPairForm />
   </Window>
