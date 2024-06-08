3ded71cd934e519b57330e360e090dbcca5e16a9
Furkan Baytekin <furkanbaytekin@gmail.com>
2024-06-09 02:06:10 +0300

    chore: ConstantPairItem effects and draggable prop

--- src/components/(settings)/ConsantPairs/ConstantPairItem.tsx
+++ src/components/(settings)/ConsantPairs/ConstantPairItem.tsx
@@ -10,8 +10,9 @@ interface IConstantPairItemProps {
 const ConstantPairItem: FC<IConstantPairItemProps> = ({ constant }) => {
 
   return <Link
+    draggable="false"
     to={`/settings/constant-pairs/${constant.key}`}
-    className="flex items-center gap-2 p-3 bg-tuatara-50 dark:bg-tuatara-900 rounded-md w-full text-left"
+    className="flex items-center gap-2 p-3 bg-tuatara-50 dark:bg-tuatara-900 rounded-md w-full text-left hover:bg-white dark:hover:bg-tuatara-800 transition-all duration-300 ease-in-out"
   >
     <div className="grid leading-none gap-2 grow">
       <div className="line-clamp-1 text-creamcan-500">
