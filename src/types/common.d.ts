11f2f0af581c2610e9747880ed001baf443b5d2e
Furkan Baytekin <furkanbaytekin@gmail.com>
2024-06-15 22:43:22 +0300

    refactor: Update types in common.d.ts for database entries

--- src/types/common.d.ts
+++ src/types/common.d.ts
@@ -1,15 +1,20 @@
-export type ListablePassphrase = {
-  readonly id?: string
+export type MicroDatabaseEntry = {
+  readonly id: string
   platform: string
-  identity: string
   url: string
-  readonly createdAt?: string
-  readonly updatedAt?: string
-  readonly lastAccessedAt?: string
-  readonly totalAccesses?: number
 }
 
-export type Passphrase = ListablePassphrase & {
+export type CountableDatabaseEntry = MicroDatabaseEntry & {
+  readonly totalAccesses: number
+}
+
+export type ListableDatabaseEntry = CountableDatabaseEntry & {
+  identity: string
+  readonly createdAt: string
+  readonly updatedAt: string
+}
+
+export type DatabaseEntry = ListableDatabaseEntry & {
   passphrase: string
   notes?: string
 }
