6dd17e62e7bd731f078ffe9faa031e59b8653909
Furkan Baytekin <furkanbaytekin@gmail.com>
2024-07-18 20:10:19 +0300

    style: line breaks

--- src/components/windows/Detective/DetectiveSheetGrid.tsx
+++ src/components/windows/Detective/DetectiveSheetGrid.tsx
@@ -10,7 +10,9 @@ interface IDetectiveSheetGridProps {
 const DetectiveSheetGrid: FC<IDetectiveSheetGridProps> = ({
   title, passphrases
 }) => <div className="p-2 flex flex-col gap-1 bg-tuatara-100 dark:bg-tuatara-950 rounded-lg">
-    {title && <em>{title}</em>}
+    {title &&
+      <em>{title}</em>
+    }
 
     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-2">
       {passphrases.map((passphrase) =>
