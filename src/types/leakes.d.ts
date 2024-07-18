f86d420108b14214445e4a2e12cf710b82dd998d
Furkan Baytekin <furkanbaytekin@gmail.com>
2024-07-18 22:25:15 +0300

    chore: Add types for leaked data

---
+++ src/types/leakes.d.ts
@@ -0,0 +1,10 @@
+export type LeakedData = {
+  readonly Name: string
+  readonly Title: string
+  readonly Domain: string
+  readonly BreachDate: string
+  readonly PwnCount: number
+  readonly Description: string
+  readonly LogoPath: string
+  readonly DataClasses: string[]
+}
