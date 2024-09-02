1956a677303f01e072236683fb3a6ef777a0fc07
Furkan Baytekin <furkanbaytekin@gmail.com>
2024-09-02 20:42:44 +0300

    chore: updated new leak data api endpoint

--- src/services/newsServices.ts
+++ src/services/newsServices.ts
@@ -5,5 +5,5 @@ import { apiCaller } from "@/helpers/apiCaller"
  * @returns LeakedData array
  */
 export const getNews = () => apiCaller<Paginated<LeakedData>>({
-  url: "https://passenger-landing.vercel.app/api/news"
+  url: "https://passenger-landing.vercel.app/api/breaches"
 })
