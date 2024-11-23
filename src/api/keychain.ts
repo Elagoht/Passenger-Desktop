9711ef35a45265a85dae5f70deb964edbda02b5a
Furkan Baytekin <furkanbaytekin@gmail.com>
2024-11-23 14:59:52 +0300

    style: keychain code simplified

--- src/api/keychain.ts
+++ src/api/keychain.ts
@@ -3,40 +3,43 @@ import { invoke } from "@tauri-apps/api/tauri"
 class KeyRing {
   public static write = async (username: string, key: string): Promise<void> => {
     try {
-      await invoke(
-        "save_key",
-        {
-          service: "dev.elagoht.passenger",
-          username: username,
-          key
-        }
-      )
+      await invoke("save_key", {
+        service: "dev.elagoht.passenger",
+        username: username,
+        key
+      })
     } catch (error) {
-      if (error instanceof Error)
-        throw new Error(`Failed to save key: ${error.message}`)
-      throw new Error("Failed to save key: Unknown error")
+      throw new Error(`Failed to save key: ${error instanceof Error
+        ? error.message
+        : "Unknown error"
+        }`
+      )
     }
   }
 
   public static read = async (username: string): Promise<Maybe<string>> => {
     try {
-      return await invoke(
-        "get_key",
-        {
-          service: "dev.elagoht.passenger",
-          username: username
-        }
-      )
+      return await invoke("get_key", {
+        service: "dev.elagoht.passenger",
+        username: username
+      })
     } catch (error) {
-      if (error instanceof Error)
-        throw new Error(`Failed to get key: ${error.message}`)
-      throw new Error("Failed to get key: Unknown error")
+      throw new Error(`Failed to save key: ${error instanceof Error
+        ? error.message
+        : "Unknown error"
+        }`
+      )
     }
   }
 
   public static generate = (): string => {
-    const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@#$%^&*()_+{}[]|\\:;\"'<>,.?/!-=~`"
     let result = ""
+
+    const characters = "0123456789"
+      + "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+      + "abcdefghijklmnopqrstuvwxyz"
+      + "@#$%^&*()_+{}[]|\\:;\"'<>,.?/!-=~`"
+
     const length = 32
     Array.from({ length }).forEach(() => {
       result += characters.charAt(
