1f1aea352a5bcc91705eba3153e082c7db9ab0c8
Furkan Baytekin <furkanbaytekin@gmail.com>
2024-12-28 01:04:36 +0300

    style: format JSX rendering for improved readability

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,10 @@ import App from "./app/App"
 import Toaster from "./components/utility/Toast/Toaster"
 import "./design/global.css"
 
-createRoot(document.getElementById("root")!).render(<>
-  <App />
+createRoot(document.getElementById("root")!).render(
+  <>
+    <App />
 
-  <Toaster />
-</>)
+    <Toaster />
+  </>
+)
